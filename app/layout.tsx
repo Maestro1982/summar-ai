6f0bfbdceacf3b1f642fc97eebd47fce05abb450
Maestro1982 <kris.tote@telenet.be>
2025-03-19 11:22:32 +0100

    Setup project in Next.js 15

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import { Source_Sans_3 as FontSans } from 'next/font/google';
+import './globals.css';
+
+const fontSans = FontSans({
+  variable: '--font-sans',
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: 'SummarAI - AI Powered PDF Summarization',
+  description:
+    'Save hours of reading time. Transform long PDFs into clear, accurate summaries in seconds with our advanced AI Technology.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${fontSans.variable} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
