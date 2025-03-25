c973a8a8a75c08f1f804b7a0f193fff01d0abf23
Maestro1982 <kris.tote@telenet.be>
2025-03-25 15:01:54 +0100

    implement clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Source_Sans_3 as FontSans } from 'next/font/google';
 import './globals.css';
 
@@ -23,14 +24,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${fontSans.variable} font-sans antialiased`}>
-        <div className='relative flex flex-col min-h-screen'>
-          <Header />
-          <main className='flex-1'>{children}</main>
-          <Footer />
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${fontSans.variable} font-sans antialiased`}>
+          <div className='relative flex flex-col min-h-screen'>
+            <Header />
+            <main className='flex-1'>{children}</main>
+            <Footer />
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
